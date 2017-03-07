7e93a7a7b7c43980937293cf589015b90123073d
FS1360472174 <1015602538@qq.com>
2017-03-07 21:34:51 +0800

    guava collection learn

---
+++ java-web/src/guavaLearn/CollectionTest.java
@@ -0,0 +1,41 @@
+package guavaLearn;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+
+public class CollectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Set<String> setVal = new HashSet<String>();
+		setVal.add("1");
+		setVal.add("2");
+		setVal.add("3");
+		System.out.println(isEqual(setVal,"1"));
+		System.out.println(isSetEqual(setVal,"1"));
+	}
+	
+	
+	private static boolean isEqual(Set<String> setVal,String val){
+		return Iterables.any(setVal, new Predicate<String>() {
+			@Override
+			public boolean apply(String input) {
+				return input.equals(val);
+			}
+			
+		});
+	}
+	
+	private static boolean isSetEqual(Set<String>setVal,String val){
+		for(String input: setVal) {
+			if(val.equals(input)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
